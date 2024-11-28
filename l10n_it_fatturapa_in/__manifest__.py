be4238a379cb96d39beb299337f805e2a4e9cd8d
Alessandro Passanante <a.passanante@abcstrategie.it>
2024-11-28 15:10:11 +0100

    Update __manifest__.py

--- l10n_it_fatturapa_in/__manifest__.py
+++ l10n_it_fatturapa_in/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     "name": "ITA - Fattura elettronica - Ricezione",
-    "version": "16.0.1.4.5",
+    "version": "16.0.1.4.6",
     "development_status": "Beta",
     "category": "Localization/Italy",
     "summary": "Ricezione fatture elettroniche",
